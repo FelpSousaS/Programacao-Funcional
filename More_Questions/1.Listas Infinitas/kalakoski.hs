3fc6a7240df90c098888a8192e8ec84edfdafbc4
Felipe Sousa <85742339+felpsousa0@users.noreply.github.com>
2021-08-17 10:33:33 -0300

    Add files via upload

---
+++ More_Questions/1.Listas Infinitas/kalakoski.hs
@@ -0,0 +1,8 @@
+kolakoski :: [Int]
+kolakoski = 1:2: drop 2 (concat . zipWith replicate kolakoski . cycle $ [1, 2])
+
+--Drop = aplicado a um número e uma lista, retorna a lista com o número especificado de elementos removidos da frente da lista. Se a lista tiver menos do que o número necessário de elementos, ela retornará [].
+--Concat = aplicado a uma lista de listas, junta-as usando o operador ++.
+--zipWith = aplicado a uma função binária e duas listas, retorna uma lista contendo elementos formados pela aplicação da função aos elementos correspondentes nas listas.
+--Replicate = dado um número inteiro (positivo ou zero) e um valor, retorna uma lista contendo o número especificado de instâncias desse valor.
+--Cycle  = cria uma lista circular a partir de uma lista finita
